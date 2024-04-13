ee0f2ef07e359644a9c76f5356b2579956323e69
hihihiihiaidi alsdalsdllt <94860954+sajdjdajdsjafjsajfasdf@users.noreply.github.com>
2024-04-13 12:53:49 -0300

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -1,4 +1,4 @@
-    --// beta build for esp for blackout gui
+    --// as of 4/13/24 this will no longer be used and will be integrated into main hub
     local Players = game:GetService("Players") -- // bypass renaming
     local Player = game.Players.LocalPlayer
     local Camera = workspace.CurrentCamera
@@ -10,7 +10,7 @@
 
     ]]
 
-    local function cleanUpCheck(index)
+    local function cleanUpCheck(index) --// i love bieng abloe t oclean uyp
         if ESP_Data[index] then
 
             ESP_Data[index]["RS"]:Disconnect()
@@ -23,6 +23,7 @@
 
         end
     end
+    --// fancy schmancy function above
 
     local function createNewEsp(Plr)
         if Plr == Player then return end
@@ -40,7 +41,7 @@
         BoxEsp.Filled = false
         local Text = Drawing.new("Text")
         Text.Text = Plr.Name
-        Text.Size = 25
+        Text.Size = 12.5
         Text.Color = Color3.fromRGB(255,255,255)
         Text.Visible = false
         Text.Center = true
@@ -52,20 +53,31 @@
         
             if Plr.Character then
                 
-                local Vector,IsOnScreen = Camera:WorldToViewportPoint(Plr.Character:GetPivot().Position)
+                if getgenv().ESPEnabled then
+                    local Vector,IsOnScreen = Camera:WorldToViewportPoint(Plr.Character:GetPivot().Position)
+                    local Root = Plr.Character:WaitForChild("HumanoidRootPart")
+                    local Head = Plr.Character:WaitForChild("Head")
+                    local RootPos,RootVis = Camera:WorldToViewportPoint(Root.Position)
+                    local HeadPos = Camera:WorldToViewportPoint(Head.Position+Vector3.new(0,0.5,0))
+                    local LegPos = Camera:WorldToViewportPoint(Root.Position-Vector3.new(0,3,0))
 
-                if IsOnScreen then --// mfw the script doesnt work because i named this is on screen and not on screen (i fuck up variables)
+                    if IsOnScreen then
 
-                    BoxEsp.Size = Vector2.new(10,25)
-                    BoxEsp.Position = Vector2.new(Vector.X-4,Vector.Y+2)
-                    BoxEsp.Visible = true
-                    Text.Position = Vector2.new(Vector.X,Vector.Y-25)
-                    Text.Visible = true
+                        BoxEsp.Size = Vector2.new(500/RootPos.Z,HeadPos.Y-LegPos.Y) --// math , math and math! credits to 0x83
+                        BoxEsp.Position = Vector2.new(RootPos.X-BoxEsp.Size.X/2,RootPos.Y-BoxEsp.Size.Y/2)
+                        BoxEsp.Visible = true
+                        Text.Position = Vector2.new(HeadPos.X,RootPos.Y-20) --// note to self: vector2 is measured in PIXELS, adding 0.5 pixels won't change much
+                                                                            --// UPDATE TO NOTE: why the fuck do i have to subtract to increase y?
+                        Text.Visible = true                                 --// reference for later: vectorToWorldSpace(self._cameraCFrame, vector3New(...)),
+                    else
+
+                        BoxEsp.Visible = false
+                        Text.Visible = false
+
+                    end
                 else
-
-                    BoxEsp.Visible = false
                     Text.Visible = false
-
+                    BoxEsp.Visible = false
                 end
             else
 
@@ -88,3 +100,4 @@ end)
 Players.PlayerRemoving:Connect(function(player)
     cleanUpCheck(player)
 end)
+--// i do NOT remember making these
